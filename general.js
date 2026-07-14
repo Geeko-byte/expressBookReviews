4661bc4201c254feb0e32665ea88af133719310f
Geeko-byte <23053684@kiit.ac.in>
2026-07-15 00:09:58 +0530

    Update general.js

--- general.js
+++ general.js
@@ -25,7 +25,7 @@ public_users.post("/register", (req,res) => {
   if (username && password) {
     if (!doesExist(username)) { 
       users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
     } else {
       return res.status(404).json({message: "User already exists!"});
     }
@@ -126,7 +126,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   getFromISBN(isbn).then(
     (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send(error)
+    (error) => res.status(404).json({ message: error })
   )
  });
 
@@ -134,25 +134,32 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Add the code for getting the book details based on Author (done in Task 3) using Promise callbacks or async-await with Axios.
 
 function getFromAuthor(author){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.author === author){
-        output.push(book_);
+  return new Promise((resolve, reject) => {
+    let output = [];
+
+    for (let isbn in books) {
+      if (books[isbn].author === author) {
+        output.push(books[isbn]);
       }
     }
-    resolve(output);  
-  })
+
+    if (output.length > 0) {
+      resolve(output);
+    } else {
+      reject("Author not found");
+    }
+  });
 }
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  getFromAuthor(author)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
+public_users.get('/author/:author', function(req, res) {
+    getFromAuthor(req.params.author)
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(404).json({ message: err });
+    });
 });
 
 // Task 13
@@ -160,25 +167,35 @@ public_users.get('/author/:author',function (req, res) {
 
 
 function getFromTitle(title){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.title === title){
-        output.push(book_);
-      }
-    }
-    resolve(output);  
-  })
+    return new Promise((resolve,reject)=>{
+
+        let output=[];
+
+        for(let isbn in books){
+            if(books[isbn].title===title){
+                output.push(books[isbn]);
+            }
+        }
+
+        if(output.length>0){
+            resolve(output);
+        }else{
+            reject("Title not found");
+        }
+
+    });
 }
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  getFromTitle(title)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
-});
+public_users.get('/title/:title',(req,res)=>{
 
+    getFromTitle(req.params.title)
+    .then(result=>{
+        res.status(200).json(result);
+    })
+    .catch(err=>{
+        res.status(404).json({message:err});
+    });
+
+});
 module.exports.general = public_users;
